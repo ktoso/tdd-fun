189505097bf89d8b97e71f58e4ed716e2abab8a9
Konrad Malawski <konrad.malawski@project13.pl>
2010-12-06 11:57:13 +0100

    added hamcrest

---
+++ day1/test/pl/pragmatists/assertions/RevertStringTest.java
@@ -0,0 +1,43 @@
+package pl.pragmatists.assertions;
+
+import org.junit.Test;
+
+import static junit.framework.Assert.*;
+
+public class RevertStringTest {
+
+  public static String revert(String word){
+    return word;
+  }
+
+  public static boolean revertable(String word){
+    return false;
+  }
+
+
+  String word = "word";
+
+  @Test
+  public void notEmptyWordIsRevertable() throws Exception {
+    assertTrue(revertable(word));
+  }
+
+  @Test
+  public void emptyWordIsNotRevertable() throws Exception {
+    assertFalse(revertable(""));
+    assertFalse(revertable(null));
+  }
+
+  @Test
+  public void revertedPalindromeIsEqualButNotSame() throws Exception {
+    String ala = "ala";
+    assertEquals(ala, revert(ala));
+    assertNotSame(ala, revert(ala));
+  }
+
+  @Test
+  public void revertedNullIsNull() throws Exception {
+    assertNull(revert(null));
+  }
+}
+
